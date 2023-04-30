d52c293f30794525017585208a700e4b9e6db056
Reaz <reaz.rahman@macaulay.cuny.edu>
2023-04-30 14:31:13 -0400

    wip

--- device_history.py
+++ device_history.py
@@ -2,6 +2,6 @@ import datetime
 
 ## static members only
 class DeviceHistory(object): 
-    is_on = False 
+    is_on = False
     last_turned_on:datetime.datetime = None # datetime object
     last_turned_off:datetime.datetime = None #datetime object
