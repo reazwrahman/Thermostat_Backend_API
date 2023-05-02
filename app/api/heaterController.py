cd6b84fb7058d11d7872eec82e1a1669a1fd31e6
Reaz <reaz.rahman@macaulay.cuny.edu>
2023-05-01 21:01:42 -0400

    add more logs and do test next time

--- app/api/heaterController.py
+++ app/api/heaterController.py
@@ -6,24 +6,23 @@ from app.api.device_history import DeviceHistory
 
 class PowerStates(Enum):
     NOT_STARTED = 1
-    ON_STATE = 2
-    OFF_STATE = 3  
+    ON_STATE = 2 
     COOL_DOWN = 4
 
 ## state machine diagram showing the different state transitions: 
-#         ____________________
-#        |                    |       
-#        x                    |
+#            ___________________
+#           |                   |       
+#           x                   |
 ## NOT_STARTED  --> ON  --> COOL_DOWN 
-#             |     ^
-#             |     |
-#             ---> OFF  
+#               |              ^
+#               |              |
+#                --------------  
 
 class HeaterController(object):
     def __init__(self, target_temp): 
-        self.cool_down_period = 10 #minutes 
-        self.last_turned_on_time:datetime = None 
-        self.last_turned_off_time:datetime = None 
+        self.cool_down_period = 10 #minutes
+
+        self.last_turned_on_time:datetime = None
         self.cool_down_started_time:datetime = None  
 
         self.pin_controller = PinController()
@@ -41,39 +40,37 @@ class HeaterController(object):
             self.__execute_on_state__()  
         
         elif self.current_state == PowerStates.COOL_DOWN: 
-            self.__execute_cool_down_state__() 
+            self.__execute_cool_down_state__()
         
-        elif self.current_state == PowerStates.OFF_STATE: 
-            self.execute_off_state__()
+        else: 
+            raise ValueError('HeaterController::execute_state_machine invalid state')
 
         
-
-
 
     def __start_state_machine__(self): 
-        current_temp = DeviceHistory.last_temperature  
+        current_temp = DeviceHistory.last_temperature
+        print ('==============================') 
+        print (f'HeaterController ::__start_state_machine__ starting state machine')  
+        print (f'current_temp = {current_temp}, target_temp= {self.target_temp}')
         ## on condition
         if self.target_temp > current_temp: 
-            if DeviceHistory.is_on: 
-                self.last_turned_on_time = DeviceHistory.last_turned_on 
-            else:  
+            print (f'HeaterController ::__start_state_machine__ target temp > current temp')   
+            if not DeviceHistory.is_on: 
                 self.pin_controller.turn_on()
                 DeviceHistory.is_on = True
                 self.last_turned_on_time = datetime.datetime.now()
-            
-            self.current_state = PowerStates.ON_STATE 
+                self.current_state = PowerStates.ON_STATE 
+                print (f'HeaterController ::__start_state_machine__ switching to ON state')    
         
         ## off condition
         else: 
             if DeviceHistory.is_on: 
                 self.pin_controller.turn_off()  
-                DeviceHistory.is_on = False
-                self.last_turned_off_time = datetime.datetime.now() 
-            else: 
-                self.last_turned_off_time = DeviceHistory.last_turned_off 
-
-            self.current_state = PowerStates.OFF_STATE 
- 
+                DeviceHistory.is_on = False 
+                self.current_state = PowerStates.COOL_DOWN 
+                self.cool_down_started_time = datetime.datetime.now() 
+                print (f'HeaterController ::__start_state_machine__ switching to COOL_DOWN state')
+        print ('==============================')
     
     def __execute_on_state__(self): 
         current_temp = DeviceHistory.last_temperature 
@@ -88,21 +85,15 @@ class HeaterController(object):
             DeviceHistory.is_on = False 
             self.current_state = PowerStates.COOL_DOWN  
             self.cool_down_started_time = datetime.datetime.now() 
-        
-        else: 
-            pass # keep chilling in this state
 
     
     def __execute_cool_down_state__(self): 
         time_now = datetime.datetime.now()
-        timedelta = time_now - self.last_turned_on_time
+        timedelta = time_now - self.cool_down_started_time
         delta_in_minutes = int(timedelta.total_seconds() / 60)  
 
         if delta_in_minutes > self.cool_down_period: 
             self.current_state = PowerStates.NOT_STARTED  
-        
-        else: 
-            pass ## keep chilling 
-    
-    def __execute_off_state__(self): 
-        
+
+ 
+
