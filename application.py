830ada47b03762f29bcb4679260edd013335667a
Reaz Rahman <reazrahman@Reazs-Laptop.local>
2024-02-29 21:24:30 -0500

    use port 80 to avoid entering port in dnsname

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 
 def app_wrapper(): 
-    app.run (host='0.0.0.0', port=5001, use_reloader=False)
+    app.run (host='0.0.0.0', port=80, use_reloader=False)
 
 if __name__ == "__main__":
     #thermo_thread = ThreadFactory.get_thread_instance("thermostat") 
